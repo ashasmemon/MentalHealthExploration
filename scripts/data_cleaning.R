32edaf7cc3876f423bf88a8665f2d56fa971287f
ashasmemon <ashasmemon@gmail.com>
2022-04-27 01:53:24 -0400

    add cleaning

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -3,4 +3,11 @@
 # Data: 27 April 2022
 
 
+library(dplyr)    
 
+
+raw_data <- readr::read_csv("../inputs/data/indicators.csv")
+sex_col <- select(filter(raw_data, Strata == "Sex"), c(1:4))
+education_col <- select(filter(raw_data, Strata == "Education"), c(1:4))
+age_col <- select(filter(raw_data, Strata == "Age"), c(1:4))
+race_col <- select(filter(raw_data, Strata == "Race-Ethnicity"), c(1:4))
